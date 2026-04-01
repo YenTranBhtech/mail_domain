80fb87c60fb6859a452c5248bc5f63dbab9a55cb
“YenTran” <yentran.bhtech@gmail.com>
2026-04-01 16:11:16 +0700

    [IMP] Upgrade module bhs_mail_domain ver 19

--- bhs_mail_domain/__manifest__.py
+++ bhs_mail_domain/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 {
     'name': "Mail Domain",
-    'version': '1.0',
+    'version': '19.0.1.0',
     'summary': 'BHS Mail',
     'category': 'Mail',
     'description': """BH Mail""",
